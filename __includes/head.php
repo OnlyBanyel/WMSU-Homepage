43ca28e95b8ed471b3cedc37f6aa066b9b5e3319
AthelstanMCCV <165026195+AthelstanMCCV@users.noreply.github.com>
2025-03-02 13:16:21 +0800

    Included Lateef and Montserrat Fonts

--- __includes/head.php
+++ __includes/head.php
@@ -9,5 +9,16 @@
         <?php require_once '../vendor/bootstrap-5.3.3-dist/css/bootstrap.min.css'; ?>
         <?php require_once '../vendor/datatable-2.1.8/datatables.min.css'; ?>
         <?php require_once '../css/navbar.css'; ?>
+        <?php require_once '../css/fonts.css'; ?>
     </style>
+
+    <?php # Lateef Fonts ?>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lateef:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+
+    <?php # Montserrat Fonts ?>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
